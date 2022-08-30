f9ce95fb0b4fcde6ec00460677b36c643b97161b
luferazth <luferazth@gmail.com>
2022-08-30 23:08:56 +0200

    Resources in aws part 1

---
+++ resources.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  access_key = XXXX
+  secret_key = XXXX
+  region = "us-east-1"
+}
+
+
+data "aws_ssm_parameter" "ami"{
+    name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource "aws_vpc" "vpc" {
+    cidr_block           = "10.0.0.0/16"
+    enable_dns_hostnames = "true"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block              = "10.0.0.0/24"
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+}
+
+resource "aws_route_table" "rtb" {
+    vpc_id = aws_vpc.vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id =  aws_internet_gateway.igw.id
+    }
+}
+resource "aws_route_table_association" "rta-subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtb.id
+}
+resource "aws_security_group" "nginx-sg" {
+  name   = "nginx_sg"
+  vpc_id = aws_vpc.vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress{
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+resource "aws_instance" "nginx1" {
+    ami                    = nonsensitive(data.aws_ssm_parameter.ami.value)
+    instance_type          = "t2.micro"
+    subnet_id              = aws_subnet.subnet1.id
+    vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+
+    user_data = <<EOF
+#!/bin/bash
+
+sudo amazon-linux-extras install -y nginx1
+sudo service nginx start
+sudo rm /usr/share/nginx/html/index.html
+echo '<html><head><title>TACO TEAM SERVER</title></head></html>' > /var/www/index.html
+EOF
+}
